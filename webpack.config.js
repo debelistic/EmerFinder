cc94857be1e690399448369c001e684e3133b287
Victor Awotidebe <victorawotidebe@gmail.com>
2019-09-16 22:52:06 +0100

    chore(webpack) - setup webpack
    - add webpack confifuration
    - add style loaders configuration
    
    [Finishes #168521030]

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
